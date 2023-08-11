4278246842726b66f0ecc1d6110191f64562162d
infawikirich <infawikirich@gmail.com>
2023-08-10 21:04:15 -0400

    This the result of the SSA model

---
+++ Africa_SSA_Model/run_model.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account def-training-wa
+#SBATCH --gpus-per-node=t4:2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=2:00:00
+
+cd /home/guest185/SPARK_Stater/Africa_SSA_Model/
+module load python/3.8
+source /home/guest185/hackathon_38/bin/activate
+
+python ssa_swin_model.py
