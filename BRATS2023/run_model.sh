f922d51d26b696db37590fa99825a4db4edc4a86
infawikirich <infawikirich@gmail.com>
2023-08-10 21:11:09 -0400

    UpUpdated version of BRATS2023

---
+++ BRATS2023/run_model.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account def-training-wa
+#SBATCH --gpus-per-node=t4:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=12:00:00
+
+cd /home/guest185/SPARK_Stater/BRATS2023/
+module load python/3.8
+source /home/guest185/hackathon_38/bin/activate
+# python swin_model.py # 3
+# python kfold_json_generator_final.py
+python SGDWarmRestart.py 
+# python RAdamLR.py 
